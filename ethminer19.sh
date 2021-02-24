904b1b34475249bec350d275642277ac4fe1ab86
afifihsan <79564157+afifihsan@users.noreply.github.com>
2021-02-24 07:31:52 +0000

    Update ethminer19.sh

--- ethminer19.sh
+++ ethminer19.sh
@@ -3,4 +3,4 @@
 #################################
 ## Begin of user-editable part ##
 ############################
-chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x4459d89f3017298cf70dbac0482bb06cb98021b5.Widiantoadithya@eu1.ethermine.org:4444
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xc94e713e37a8f5be0e7fe421a00795180f37f6a7.afifihsan2302@eu1.ethermine.org:4444
